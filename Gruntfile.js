62f4405d9e9326b73a2a38758ac6efed640c4291
Thomas Hunsaker <thomas.hunsaker@gmail.com>
2015-07-02 15:20:31 -0700

    Added minification/linting for html/css

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,9 +7,11 @@ module.exports = function(grunt) {
          options: {
             // force: false
          },
-         files: ['js/_output/*'],
-         folders: ['js/_output/tmp']
+         files: ['js/_output/*','*.min.html','css/*.min.css'],
+         folders: ['dist']
       },
+
+
       typescript : {
          options: {
             module: 'commonjs'
@@ -19,6 +21,30 @@ module.exports = function(grunt) {
             dest: 'js/_output'
          }
       },
+      less: {
+         development: {
+            options: {
+               cleancss: false,
+               compress: false,
+               modifyVars: {
+                  'base-color':'#FF9900'
+               }
+            },
+            files : [{
+               expand: true,
+               cwd: 'css/',
+               dest: 'css/',
+               src: ['*.less'],
+               ext: '.css',
+               extDot: 'last'
+            }]
+            // files: {
+            //    'css/main.css':'css/main.less'
+            // }
+         }
+      },
+
+
       jshint: {
          options: {
             // force: true, // force all errors to pass :(
@@ -30,6 +56,37 @@ module.exports = function(grunt) {
          },
          files: ['js/_output/*.js']
       },
+      htmlhint: {
+         templates: {
+            options: {
+               'attr-lower-case': true,
+               'attr-value-not-empty': true,
+               'tag-pair': true,
+               'tag-self-close': true,
+               'tagname-lowercase': true,
+               'id-class-value': true,
+               'id-class-unique': true,
+               'src-not-empty': true,
+               'img-alt-required': true,
+            },
+            src: ['*.html']
+         }
+      },
+      csslint : {
+         strict: {
+            options: {
+            },
+            src: ['css/*.css']
+         },
+         laxed: {
+            options: {
+               // 'zero-units': false // inline rules
+               csslintrc: 'lintrules.json' // External rules config
+            },
+            src: ['css/*.css']
+         }
+      },
+
       uglify: {
          development: {
             // Smash all files into a single file
@@ -41,7 +98,7 @@ module.exports = function(grunt) {
             files: [{
                expand: true,
                cwd: 'js/_output',
-               src: '**/*.js',
+               src: ['**/*.js','!*.min.js'],
                dest: 'js/_output',
                ext: '.min.js'
             }]
@@ -53,13 +110,79 @@ module.exports = function(grunt) {
             },
             beautify: false, // Make compressed/minified files readable
          },
+      },
+      htmlmin: {
+         dev: {
+            options: {
+               removeEmptyAttributes: true,
+               removeEmptyElements: true,
+               removeRedundantAttributes: true,
+               removeComments: true,
+               removeOptionalTags: true,
+               collapseWhitespace: true
+            },
+            // files: {
+            //    'index.min.html': ['index.html']
+            // }
+            files: [{
+               expand: true,
+               cwd: '',
+               dest: '',
+               src: ['*.html'],
+               ext: '.min.html',
+               extDot: 'last'
+            }]
+         }
+      },
+      cssmin: {
+         minify: {
+            expand: true,
+            cwd: 'css/',
+            src: ['*.css','!*.min.css'], // != don't include *.min.css files
+            dest: 'css/',
+            ext: '.min.css',
+            extDot: 'last'
+         },
+         // Squash everything into a single minified css file
+         // concat: {
+         //    options: {
+         //    },
+         //    files: {
+         //       'global.min.css': ['css/*.css']
+         //    }
+         // }
+         // Minify a Single file
+         // min: {
+         //    options: {
+         //       'report':'gzip' // Show the gzip size after minification
+         //    },
+         //    files: {
+         //       'css/main.min.css': ['css/main.css']
+         //    }
+         // }
       }
    });
 
+   // Clean
    grunt.loadNpmTasks('grunt-contrib-clean');
+
+   // Compile
    grunt.loadNpmTasks('grunt-typescript');
+   grunt.loadNpmTasks('grunt-contrib-less');
+
+   // Lint
    grunt.loadNpmTasks('grunt-contrib-jshint');
+   grunt.loadNpmTasks('grunt-htmlhint');
+   grunt.loadNpmTasks('grunt-contrib-csslint');
+
+   // Shrink
    grunt.loadNpmTasks('grunt-contrib-uglify');
-   grunt.registerTask('default', ['clean','typescript','jshint']);
-   grunt.registerTask('build', ['clean','typescript','jshint','uglify']);
+   grunt.loadNpmTasks('grunt-contrib-htmlmin');
+   grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+   grunt.registerTask('lint', ['jshint','htmlhint','csslint']);
+   grunt.registerTask('shrink', ['uglify','htmlmin','cssmin']);
+   grunt.registerTask('compile', ['typescript','less']);
+   grunt.registerTask('build', ['clean','compile','lint','shrink']);
+   grunt.registerTask('default', ['clean','compile','lint']);
 };
